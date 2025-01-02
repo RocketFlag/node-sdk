499d5bed69ecb2daea04cd027370286dd64e0770
JK Gunnink <jgunnink@gmail.com>
2025-01-02 23:36:33 +0800

    improve test cases and add new ones

--- index.test.ts
+++ index.test.ts
@@ -7,29 +7,15 @@ global.fetch = jest.fn() as jest.Mock<Promise<Response>>;
 describe("createRocketflagClient", () => {
   const apiUrl = "https://api.rocketflag.app";
   const flagId = "test-flag";
-  const userContext = { userId: "user123" };
+  const userContext = { cohort: "user123" };
 
   beforeEach(() => {
     jest.resetAllMocks();
     jest.spyOn(console, "error").mockImplementation(jest.fn());
   });
 
-  it("should create a client with default options", () => {
-    const client = createRocketflagClient();
-    expect(client).toBeDefined();
-    expect(client.getFlag).toBeDefined();
-    expect(client.getFlag).toBeInstanceOf(Function);
-  });
-
-  it("should create a client with custom options", () => {
-    const client = createRocketflagClient("v2", "https://example.com/api");
-    expect(client).toBeDefined();
-    expect(client.getFlag).toBeDefined();
-    expect(client.getFlag).toBeInstanceOf(Function);
-  });
-
   describe("getFlag", () => {
-    it("should fetch and cache a flag", async () => {
+    it("should fetch a flag", async () => {
       const mockFlag: FlagStatus = { name: "Test Flag", enabled: true, id: flagId };
       (fetch as jest.Mock).mockResolvedValue({ ok: true, json: () => Promise.resolve(mockFlag) });
 
@@ -37,13 +23,53 @@ describe("createRocketflagClient", () => {
       const flag = await client.getFlag(flagId, userContext);
 
       expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenCalledWith(new URL(`${apiUrl}/v1/flags/${flagId}?userId=${userContext.userId}`), { method: "GET" });
+      expect(fetch).toHaveBeenCalledWith(new URL(`${apiUrl}/v1/flags/${flagId}?cohort=${userContext.cohort}`), { method: "GET" });
       expect(flag).toEqual(mockFlag);
+    });
 
-      // Fetch again to test caching
-      const cachedFlag = await client.getFlag(flagId, userContext);
-      expect(fetch).toHaveBeenCalledTimes(1); // Still only called once
-      expect(cachedFlag).toEqual(mockFlag);
+    it("should fetch a flag with special characters in the query", async () => {
+      userContext.cohort = "user+testing_rocketflag@example.com";
+      const mockFlag: FlagStatus = { name: "Test Flag", enabled: true, id: flagId };
+      (fetch as jest.Mock).mockResolvedValue({ ok: true, json: () => Promise.resolve(mockFlag) });
+
+      const client = createRocketflagClient();
+      const flag = await client.getFlag(flagId, userContext);
+
+      expect(fetch).toHaveBeenCalledTimes(1);
+
+      const expectedUrl = `${apiUrl}/v1/flags/${flagId}?cohort=${"user%2Btesting_rocketflag%40example.com"}`;
+      const expectedURLObject = new URL(expectedUrl);
+
+      expect(fetch).toHaveBeenCalledWith(expect.objectContaining({ href: expectedURLObject.href }), { method: "GET" });
+      expect(flag).toEqual(mockFlag);
+    });
+
+    describe("custom client options", () => {
+      it("can create a client with a custom version", async () => {
+        const mockFlag: FlagStatus = { name: "Test Flag", enabled: true, id: flagId };
+        (fetch as jest.Mock).mockResolvedValue({ ok: true, json: () => Promise.resolve(mockFlag) });
+
+        const client = createRocketflagClient("v2");
+        await client.getFlag(flagId);
+
+        const expectedUrl = `${apiUrl}/v2/flags/${flagId}`;
+        const expectedURLObject = new URL(expectedUrl);
+
+        expect(fetch).toHaveBeenCalledWith(expect.objectContaining({ href: expectedURLObject.href }), { method: "GET" });
+      });
+
+      it("can create a client with a custom url", async () => {
+        const mockFlag: FlagStatus = { name: "Test Flag", enabled: true, id: flagId };
+        (fetch as jest.Mock).mockResolvedValue({ ok: true, json: () => Promise.resolve(mockFlag) });
+
+        const client = createRocketflagClient("v2", "https://example.com");
+        await client.getFlag(flagId);
+
+        const expectedUrl = `https://example.com/v2/flags/${flagId}`;
+        const expectedURLObject = new URL(expectedUrl);
+
+        expect(fetch).toHaveBeenCalledWith(expect.objectContaining({ href: expectedURLObject.href }), { method: "GET" });
+      });
     });
 
     it("should handle non-ok responses from the server", async () => {
